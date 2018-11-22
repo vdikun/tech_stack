5c21508d950b4749180469dbdeba6242138f711d
Vera Cat <vera@brave.coop>
2018-11-22 13:32:00 -0800

    selectionReducer worx

--- src/reducers/selectionReducer.js
+++ src/reducers/selectionReducer.js
@@ -1,3 +1,21 @@
-export default () => {
-    return [];
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'SELECT_LIBRARY':
+            const libraryId = action.payload;
+            var index = state.indexOf(libraryId);
+            var newState;
+            if (index == -1) {
+                // add libraryId to array
+                newState = [ ...state, libraryId ];
+            }
+            else {
+                // remove libraryId from array
+                newState = state;
+                newState.splice(index, 1);
+            }
+            console.log(newState);
+            return newState;
+        default:
+            return state;
+    }
 };
