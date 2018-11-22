8f49398c8448bdc33988bbcbf057d149a2c32088
Vera Cat <vera@brave.coop>
2018-11-22 12:08:52 -0800

    library list gets libraries as props

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 
 class LibraryList extends Component {
     render() {
+        console.log(this.props);
         return <View />;
     }
 }
 
 const mapStateToProps = state => {
-    console.log(state);
-    return state;
+    return { libraries: state.libraries };
 };
 
 export default connect(mapStateToProps)(LibraryList);
