b3f4e5b1b915164caf8d9e7c7e811543646e3818
Vera Cat <vera@brave.coop>
2018-11-22 10:48:46 -0800

    libraryReducer boilerplate

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import libraryReducer from './libraryReducer';
 
 export default combineReducers({
-    libraries: () => []
+    libraries: libraryReducer
 });
