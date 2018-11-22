9c01cbc2729d4fd629a5414299464a5f82e6c270
Vera Cat <vera@brave.coop>
2018-11-22 11:59:27 -0800

    library list boilerplate

---
+++ src/components/LibraryList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+    render() {
+        return <View />;
+    }
+}
+
+const mapStateToProps = state => {
+    console.log(state);
+    return state;
+};
+
+export default connect(mapStateToProps)(LibraryList);
