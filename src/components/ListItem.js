17296901965d2aae6ffc4bbfcbb3b5c328d886f5
Vera Cat <vera@brave.coop>
2018-11-22 16:01:53 -0800

    buggy library description expansion

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,28 +7,44 @@ import styles from './styles/ListItem';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+
+    renderDescription() {
+        const { library, selectedLibraries } = this.props;
+        const { description, id } = library;
+        //if (selectedLibraries.includes(id)) {
+        if (selectedLibraries.indexOf(id) > -1) {
+            console.log("showing description for library " + id);
+            return (
+                <Text>{description}</Text>
+            );
+        }
+    }
+
     render() {
         const { titleStyle } = styles;
+        // selectLibrary is an action given to this.props by connect(actions)
         const { selectLibrary, library } = this.props;
         const { id, title } = library;
 
-        // props contains selectLibrary action
-        console.log(this.props);
+        console.log("rendering library " + id);
 
         return (
-            <TouchableWithoutFeedback
-                onPress={() => selectLibrary(id)}
-            >
+            <TouchableWithoutFeedback onPress={() => selectLibrary(id)}>
                 <View>
                     <CardSection>
                         <Text style = { titleStyle }>
                             { title }
                         </Text>
                     </CardSection>
+                    {this.renderDescription()}
                 </View>
             </TouchableWithoutFeedback>
         )
     }
+};
+
+const mapStateToProps = state => {
+    return { selectedLibraries: state.selectedLibraries }
 };
 
 // TODO: make library proptype for better type checking
@@ -36,5 +52,5 @@ ListItem.propTypes = {
     library: PropTypes.object.isRequired
 };
 
-// connect makes actions available as props to ListItem
-export default connect(null, actions)(ListItem);
+// connect makes actions available to this.props
+export default connect(mapStateToProps, actions)(ListItem);
